3f7f4c2ff60ea8ecbf3cef9f4875426cc63a93d1
lozenge <popich.red@gmail.com>
2023-12-29 14:47:31 +0200

    fix(locations - views.py): import permissions. Add context

--- locations/views.py
+++ locations/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Location
 from .serializers import LocationSerializer
+from shs_drf.permissions import IsAdminOrReadOnly
 
 
 class LocationList(APIView):
@@ -12,7 +13,11 @@ class LocationList(APIView):
     """
     def get(self, request):
         locations = Location.objects.all()
-        serializer = LocationSerializer(locations, many=True)
+        serializer = LocationSerializer(
+            locations,
+            many=True,
+            context={'request': request}
+        )
         return Response(serializer.data)
 
 
@@ -21,22 +26,31 @@ class LocationDetail(APIView):
     Return a single Location
     """
     serializer_class = LocationSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_object(self, pk):
         try:
             location = Location.objects.get(pk=pk)
+            self.check_object_permissions(self.request, location)
             return location
         except Location.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         location = self.get_object(pk)
-        serializer = LocationSerializer(location)
+        serializer = LocationSerializer(
+            location,
+            context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         location = self.get_object(pk)
-        serializer = LocationSerializer(location, data=request.data)
+        serializer = LocationSerializer(
+            location,
+            data=request.data,
+            context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
