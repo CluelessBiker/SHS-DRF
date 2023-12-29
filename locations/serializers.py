2124205f3e3d250843da5b70130c1128121d5c80
lozenge <popich.red@gmail.com>
2023-12-29 14:59:42 +0200

    fix(locations - serializers.py): restrict uploaded image size

--- locations/serializers.py
+++ locations/serializers.py
@@ -4,7 +4,26 @@ from .models import Location
 class LocationSerializer(serializers.ModelSerializer):
     is_admin = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        """
+        Check image size
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
     def get_is_admin(self, obj):
+        """
+        Check if user is an admin user
+        """
         request = self.context['request']
         return request.user == request.user.is_staff
 
