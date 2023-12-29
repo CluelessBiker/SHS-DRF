6525a9abd1ca2e6d270577a69e8800ce7e3ea453
lozenge <popich.red@gmail.com>
2023-12-29 16:35:21 +0200

    fix(locations - views.py): implement CREATE & DELETE functions

--- locations/views.py
+++ locations/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Location
@@ -11,6 +11,12 @@ class LocationList(APIView):
     """
     List all locations
     """
+    serializer_class = LocationSerializer
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly,
+    ]
+
     def get(self, request):
         locations = Location.objects.all()
         serializer = LocationSerializer(
@@ -19,6 +25,28 @@ class LocationList(APIView):
             context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Create a new location
+        """
+        serializer = LocationSerializer(
+            data=request.data, context={'request': request}
+        )
+
+        if serializer.is_valid():
+            if not request.user.is_staff:
+                return Response(
+                    {'detail': 'You do not have permission to perform this action'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            serializer.save()
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.data, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class LocationDetail(APIView):
@@ -45,6 +73,9 @@ class LocationDetail(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
+        """
+        Update an existing location
+        """
         location = self.get_object(pk)
         serializer = LocationSerializer(
             location,
@@ -55,3 +86,13 @@ class LocationDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Delete an existing location
+        """
+        location = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
