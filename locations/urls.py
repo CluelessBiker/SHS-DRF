529217129928d012fbe7f280cbf3ff2074de0605
lozenge <popich.red@gmail.com>
2023-12-29 14:10:36 +0200

    fic(locations - views.py): add url to location details

--- locations/urls.py
+++ locations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from locations import views
 
 urlpatterns = [
-    path('locations/', views.LocationList.as_view(),
+    path('locations/', views.LocationList.as_view()),
+    path('locations/<int:pk>', views.LocationDetail.as_view()),
 ]
