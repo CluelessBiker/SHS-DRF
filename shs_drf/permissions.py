3daaa3d0fb34d95f26d5cb2603c5dd0b9faa1af5
lozenge <popich.red@gmail.com>
2023-12-29 16:36:12 +0200

    fix(shs_drf - permissions.py): amend malfunctioning permission

--- shs_drf/permissions.py
+++ shs_drf/permissions.py
@@ -1,11 +1,12 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     """
     Provide CRUD to any Admin users.
     """
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
             return True
-        return request.user == request.user.is_staff
+        else:
+            return request.user.is_staff
