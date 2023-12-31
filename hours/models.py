c8b3df6518946cfcdf7700b9a9f34095589db18b
lozenge <popich.red@gmail.com>
2023-12-31 15:38:07 +0200

    fix(hours - models.py): add fields to model

--- hours/models.py
+++ hours/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from locations.models import Location
 
-# Create your models here.
+
+class Hour(models.Model):
+    """
+    Set hours of operation for Location models
+    """
+    dayOfWeek = [
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+        ('sat', 'Saturday'),
+        ('sun', 'Sunday'),
+    ]
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    day = models.CharField(
+        max_length=10,
+        choices=dayOfWeek,
+        default='mon',
+        blank=False,
+        null=False
+    )
+    open = models.TimeField()
+    close = models.TimeField()
+    info = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        """
+        display text
+        """
+        return f"{self.day}: {self.open} - {self.close}"
