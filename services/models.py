d4d6980e070eaa620cf860c1695471822bd9abd6
lozenge <popich.red@gmail.com>
2024-01-01 11:46:13 +0200

    fix(services - models.py): define fields

--- services/models.py
+++ services/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from locations.models import Location
 
-# Create your models here.
+
+class Service(models.Model):
+    """
+    Services available
+    """
+    locations = models.ManyToManyField(Location)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    blurb = models.CharField(max_length=250, blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    duration = models.TimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        """ Ordering Services by title """
+        ordering = ['title']
+
+    def __str__(self):
+        """ Display text """
+        return f"{self.title}"
+
+
