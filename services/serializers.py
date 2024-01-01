097607a753784067b861db388e22ceaefaa7c5de
lozenge <popich.red@gmail.com>
2024-01-01 12:26:42 +0200

    fix(services - serializers.py): add checks for image size

--- services/serializers.py
+++ services/serializers.py
@@ -7,6 +7,22 @@ class ServiceSerializer(serializers.ModelSerializer):
     """
     Serializer for Service models
     """
+    def validate_image(self, value):
+            """
+            Check image size
+            """
+            if value.size > 2 * 1024 * 1024:
+                raise serializers.ValidationError('Image size larger than 2MB!')
+            if value.image.height > 4096:
+                raise serializers.ValidationError(
+                    'Image height larger than 4096px!'
+                )
+            if value.image.width > 4096:
+                raise serializers.ValidationError(
+                    'Image width larger than 4096px!'
+                )
+            return value
+
     class Meta:
         model = Service
         fields = [
