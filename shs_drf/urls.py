0634f383d4e4b9869a9b4da0de0141ea43ba352b
lozenge <popich.red@gmail.com>
2024-01-01 11:45:02 +0200

    fix(shs_drf - urls.py): add paths for services app

--- shs_drf/urls.py
+++ shs_drf/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('locations.urls')),
     path('', include('hours.urls')),
+    path('', include('services.urls')),
 ]
