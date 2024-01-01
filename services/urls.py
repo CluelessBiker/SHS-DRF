732453dc04636308a5eebf396694e9031f18afd9
lozenge <popich.red@gmail.com>
2024-01-01 11:47:21 +0200

    feat(services - urls.py): add paths

---
+++ services/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from services import views
+
+urlpatterns = [
+    path('services/', views.ServiceList.as_view()),
+    path('services/<int:pk>', views.ServiceDetail.as_view()),
+]
