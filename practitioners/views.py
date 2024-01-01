8f1dbdf5e5c4f2baf0739b2caf25f9bf8832d7e4
lozenge <popich.red@gmail.com>
2024-01-01 12:25:29 +0200

    fix(practitioners - views.py): implement full CRUD

--- practitioners/views.py
+++ practitioners/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from shs_drf.permissions import IsAdminOrReadOnly
+from .models import Practitioner
+from .serializers import PractitionerSerializer, PractitionerDetailSerializer
 
-# Create your views here.
+
+class PractitionerList(generics.ListCreateAPIView):
+    """
+    List Practitioners & create an entry if logged-in & admin
+    """
+    serializer_class = PractitionerSerializer
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Practitioner.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Create a new Practitioner
+        """
+        serializer.save()
+
+
+class PractitionerDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve/update/delete an entry
+    if looged in & admin
+    """
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    serializer_class = PractitionerDetailSerializer
+    queryset = Practitioner.objects.all()
+
