45629e864761328cfaf0a2b3168e74a8760e88df
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-01-03 18:17:56 +0200

    fix(contact - serializers.py): implement serializers

---
+++ contact/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the contact model
+    """
+
+    class Meta:
+        model = Contact
+        fields = [
+            'id',
+            'createdAt',
+            'name',
+            'phone',
+            'email',
+            'subject',
+            'message',
+        ]
