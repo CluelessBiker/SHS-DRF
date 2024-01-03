bfb090fb1330c967f645ccd063e8657afe6ffdc5
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-01-03 18:18:40 +0200

    fix(contact - views.py): provide CRD functionality

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import generics, status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Contact
+from .serializers import ContactSerializer
+from shs_drf.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class ContactList(generics.ListCreateAPIView):
+    """
+    Restrict viewing of messages to Admin only
+    Creation of contact message can be done by any one.
+    """
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Contact.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        submit a message
+        """
+        serializer.save()
+
+
+class ContactDetail(generics.RetrieveDestroyAPIView):
+    """
+    Admin can retrieve all messages
+    Admin can delete all messages
+    Update not available.
+    """
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
