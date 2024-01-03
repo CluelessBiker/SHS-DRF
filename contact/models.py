bf6695c5aeadcd8bb0cee2cd162eee96aec351de
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-01-03 18:17:05 +0200

    feat(contact - models.py): implement form fields

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact form
+    """
+    createdAt = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    subject = models.CharField(max_length=100, blank=False, null=False)
+    message = models.TextField()
+    email = models.CharField(max_length=100, blank=False, null=False)
+    phone = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        """ Ordering messages by date created """
+        ordering = ['-createdAt']
+
+    def __str__(self):
+        """
+        Formatted display text
+        """
+        return f"Sender: {self.name}; Subject: {self.subject}"
