478de5b267758c03af503908eeed651e8f9a37d6
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-01-04 16:21:35 +0200

    fix(shs_drf - urls.py): add endpoint paths for installed packages

--- shs_drf/urls.py
+++ shs_drf/urls.py
@@ -20,6 +20,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls')
+    )
     path('', include('contact.urls')),
     path('', include('locations.urls')),
     path('', include('hours.urls')),
