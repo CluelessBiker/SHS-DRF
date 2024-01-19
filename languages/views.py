4128ab08f154e92f5853ff339780a201c7d94581
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-01-19 17:42:52 +0200

    feat(languages - views.py): provide CRUD functionality

--- languages/views.py
+++ languages/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Language
+from languages.serializers import LanguageSerializer
+from shs_drf.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class LanguageList(generics.ListCreateAPIView):
+    """
+    List languages or create an entry if logged in & admin
+    """
+    serializer_class = LanguageSerializer
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly,
+    ]
+    queryset = Language.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Create a new Language
+        """
+        serializer.save()
+
+
+class LanguageDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve/Update/Delete an entry if logged in & admin
+    """
+    permission_classes = [
+        IsAdminOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    serializer_class = LanguageSerializer
+    queryset = Language.objects.all()
