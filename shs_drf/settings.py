191ddba9b984fd4faba1bae78d071de2cb594bf7
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-03-04 15:53:40 +0200

    chore(settings): clean-up dead code

--- shs_drf/settings.py
+++ shs_drf/settings.py
@@ -109,20 +109,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN')
-#     ]
-# else:
-#     CORS_ALLOWED_ORIGIN_REGEXES = [
-#         r"^https://.*\.gitpod\.io$",
-#     ]
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN'),
-#         os.environ.get('CLIENT_ORIGIN_DEV'),
-#     ]
-
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
