214dfe7124a8268ed5c5d8372e58b95703538f99
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-17 17:19:26 +0300

    fix(settings.py): modify

--- shs_drf/settings.py
+++ shs_drf/settings.py
@@ -99,9 +99,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
