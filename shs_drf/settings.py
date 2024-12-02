8d9dbf17ed549110c43919355714703be831f275
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-12-02 09:52:58 +0200

    fix(settings.py): update pagination to 30

--- shs_drf/settings.py
+++ shs_drf/settings.py
@@ -34,9 +34,9 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-#     'DEFAULT_PAGINATION_CLASS':
-#         'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30,
 }
 
 if 'DEV' not in os.environ:
