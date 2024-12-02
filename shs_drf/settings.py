5258a41e9447c8d804549757e8de8869294be7da
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-12-02 09:57:37 +0200

    fix(settings.py): add line to middleware

--- shs_drf/settings.py
+++ shs_drf/settings.py
@@ -109,6 +109,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
